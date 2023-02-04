0fbd5e14b520902b01cf338902d6ed4f77e541aa
Benedicte44 <benedicte.cheval44@yahoo.fr>
2023-02-04 14:47:52 +0100

    slideshow construction

--- src/components/SlideShow/index.jsx
+++ src/components/SlideShow/index.jsx
@@ -1,10 +1,41 @@
 // We define here the slide show component
-function Slideshow({ image }) {
+import { useState } from 'react'
+import arrowBack from '../../assets/arrow-back.svg'
+import arrowNext from '../../assets/arrow-next.svg'
+
+function Slideshow({ pictures }) {
+  const [currentIndex, discoverNewPicture] = useState(0)
+  const nbOfPictures = pictures.length
+  const currentPicture = pictures[currentIndex]
+  const nextPicture = () => {
+    discoverNewPicture(currentIndex === nbOfPictures - 1 ? 0 : currentIndex + 1)
+  }
+  const previousPicture = () => {
+    discoverNewPicture(currentIndex === 0 ? nbOfPictures - 1 : currentIndex - 1)
+  }
+
   return (
     <div className="slideshow">
-      <img src={image} alt="" className="" />
+      <img src={currentPicture} alt="logement" />
+      {nbOfPictures > 1 && (
+        <div>
+          <span className="arrowBack" onClick={previousPicture}>
+            <img src={arrowBack} alt="go to previous" />
+          </span>
+          <span className="arrowNext" onClick={nextPicture}>
+            <img src={arrowNext} alt="go to next" />
+          </span>
+        </div>
+      )}
+      <div className="picturesDiag">
+        {currentIndex + 1}/{nbOfPictures}
+      </div>
     </div>
   )
 }
 
 export default Slideshow
+
+// {pictures.map((picture) => {
+//     return <img src={picture} alt="logement" />
+//   })}
--- src/pages/AccomodationDetails/index.jsx
+++ src/pages/AccomodationDetails/index.jsx
@@ -1,5 +1,12 @@
 // our detailed product page component
+import { useParams } from 'react-router-dom'
+import Slideshow from '../../components/SlideShow'
+import { accomodationsList } from '../../datas/accomodationList'
 
-function AccomodationDetails() {}
+function AccomodationDetails() {
+  const { id } = useParams()
+  const targetedRenting = accomodationsList.find((renting) => renting.id === id)
+  return <Slideshow pictures={targetedRenting.pictures} />
+}
 
 export default AccomodationDetails
